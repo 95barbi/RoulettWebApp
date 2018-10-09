671b11926f0b2e58a4c3a73d3e76569f41afa6c1
Borbély Bálint <borbely.balint@lippert.hu>
2018-10-09 17:10:47 +0200

    Daily commit, Game.java updated
    Updated GameTable layout

--- src/main/java/org/thesis/roulett/Populator.java
+++ src/main/java/org/thesis/roulett/Populator.java
@@ -32,19 +32,20 @@ public class Populator {
 
 	public Populator() {
 		super();
-		
+
 		random = new Random();
 	}
-	
+
 	public LocalDate randomDate() {
 		return LocalDate.now().minus(Period.ofDays((new Random().nextInt(365 * 70))));
 	}
 
 	@PostConstruct
 	public void init() {
-		firstnames = Arrays.asList("B�lint", "Kata", "Jani", "Feri", "Viktor", "Bal�zs", "Attila", "G�bor", "Andr�s", "Marci",
-				"�gi", "S�ri", "Eszter", "R�ka");
-		lastnames = Arrays.asList("Kov�cs", "Kocsis", "Szab�", "Borb�ly", "Nagy", "Honti", "Gyop�r", "T�szegi", "Arat�");
+		firstnames = Arrays.asList("Balint", "Kata", "Jani", "Feri", "Viktor", "Balazs", "Attila", "Gabor", "Andras",
+				"Marci", "Agi", "Sari", "Eszter", "Reka");
+		lastnames = Arrays.asList("Kovacs", "Kocsis", "Szabo", "Borbely", "Nagy", "Honti", "Gyopar", "Toszegi",
+				"Arato");
 
 		usernames = Arrays.asList("vaulter", "changtse", "kyanite", "carefree", "rem", "frosty", "boxing", "chowder",
 				"rocking", "pussface", "endless", "arede", "rentot");
@@ -53,27 +54,27 @@ public class Populator {
 			String name = lastnames.get(random.nextInt(lastnames.size())) + " "
 					+ firstnames.get(random.nextInt(firstnames.size()));
 			String username = usernames.get(random.nextInt(usernames.size()));
-			String password = usernames.get(random.nextInt(usernames.size()));
+			String password = "123456"+username;
 			String email = username + "@gmail.com";
 			int balance = random.nextInt(1000);
-			
+
 			Player player = new Player(name, username, email, password, balance, "USER");
-			
+
 			System.out.println("Player: " + player.toString());
 
 			playerRepository.save(player);
 		}
 
-		for (int j=0; j<50; ++j) {
+		for (int j = 0; j < 50; ++j) {
 			boolean isLost = random.nextBoolean();
 			String date = randomDate().toString();
 			int bet = random.nextInt(1000);
-			
+
 			Player player = playerRepository.findAll().get(random.nextInt(playerRepository.findAll().size()));
 			Game game = new Game(player.getName(), date, isLost, bet, player);
-			
+
 			gameRepository.save(game);
 		}
-		
+
 	}
 }
